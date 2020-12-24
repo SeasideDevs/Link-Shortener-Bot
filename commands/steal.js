e1a3d911871e0918bc38461def8c3adc05eda7a4
TheLimifiedLime <thelimifiedlime@protonmail.com>
2020-12-24 01:43:44 +0000

    I dont fucking knowwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww

--- commands/steal.js
+++ commands/steal.js
@@ -8,29 +8,23 @@ module.exports = {
     usage: '',
     category: "fun",
     execute(msg, args, client, config, prefix, axios, Discord, avatar, blacklist) {
-        console.log(msg.content)
         const limit = 10;
-        const idPattern = /[0-9]{18}/g
-        const namePattern = /<:[A-z]{1,32}:/g
-        const animatedNamePattern = /a:[A-z]{1,32}:/g
-        
-        const ids = msg.content.match(idPattern)
-        const names = msg.content.match(namePattern)
-        const animatedNames = msg.content.match(animatedNamePattern)
-        if (ids.length > limit) {
-          return msg.channel.send(`Sorry but I cannot steal more than ${limit} emojis at once. Please try again`)
+        const emotePattern = /<(a|):[A-z]{1,32}:[0-9]{18}>/g
+        //const idPattern = /[0-9]{18}/g
+        //const namePattern = /<:[A-z]{1,32}:/g
+        //const animatedNamePattern = /a:[A-z]{1,32}:/g
+
+        const emotes = msg.content.match(emotePattern);
+        if (emotes.length > limit) {
+          return msg.channel.send(`Sorry but I cannot steal more than ${limit} emotes at once. Please try again with 10 or less emotes`)
         }
 
-        for (const match of animatedNamePattern) {
-          match.replace('#', '')
-        }
-
+        console.log(emotes)
         const embed = new Discord.MessageEmbed()
         .setColor(config.mainColor)
-        .setTitle(`Emoji Results`)
-        .addField(`IDs`, `${ids.join("\n")}`)
-        .addField(`Normal Emojis`, `${names.join("\n")}`)
-        .addField(`Animated Emojis`, `${animatedNames.join("\n")}`)
+        .setTitle(`Emote Results`)
+        .addField(`Total Emotes:`, `${emotes.length}`)
+        .addField(`Emotes`, ` \`\`\`${emotes.join("\n\\")}\`\`\` `)
 
 
         msg.channel.send(embed)
