6919d2024a4d0573f555ff01c54e69c524a4e727
TheLimifiedLime <thelimifiedlime@protonmail.com>
2021-02-02 17:46:03 +0000

    Stuff

--- commands/purge.js
+++ commands/purge.js
@@ -9,31 +9,12 @@ module.exports = {
   usage: "",
   category: "moderation",
   execute(msg, args, client, config, prefix, axios, Discord, avatar, database) {
+    const member = msg.guild.member(msg.author)
+    if (!member.hasPermission(`MANAGE_MESSAGES`)) {
+      msg.channel.send(`This command can only be used my mods`)
+    }
     const amount = parseInt(args[0]);
 
-    msg.delete();
-    const bulkDelete = (amount) => {
-      msg.channel
-        .bulkDelete(amount, true)
-        .then(async function (messages) {
-          let m;
-          // This if else checks if the amount is more than 1 and updates the message to be plural
-          if (amount === 1) {
-            m = await msg.channel.send(`Deleted ${messages.size} message`);
-          } else {
-            m = await msg.channel.send(`Deleted ${messages.size} messages`);
-          }
-          m.react("785686207597379615");
-          m.delete({ timeout: 5000 });
-        })
-        .catch(console.error);
-    };
-
-    const checkAmount = () => {
-      if (amount > 100) {
-        amount -= 100;
-        bulkDelete(100);
-      }
-    };
+    
   },
 };
