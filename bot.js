e2ad5cadb13e97a230d1c01a94caa8b612cdb97a
TheLimifiedLime <thelimifiedlime@protonmail.com>
2021-02-08 15:57:33 +0000

    Prettify

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ require("toml-require").install({ toml: require("toml") });
 const config = require("./config.toml");
 const prefix = config.prefix;
 const status = {
-  activity: { name: "you", type: "WATCHING" },
+  activity: { name: "code", type: "WATCHING" },
   status: "online",
 };
 const chalk = require("chalk");
