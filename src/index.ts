02b3125a9dc3b6ea2e135393fd6380cea07a0c6c
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-03-30 15:27:54 +0000

    Run checks first thing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-import fs from "fs";
-import { parse } from "@iarna/toml";
-const config = parse(
-  fs.readFileSync(__dirname + "/config.toml", {
-    encoding: "utf-8",
-  })
-);
+import { checks } from "./core/bootchecks";
+checks();
