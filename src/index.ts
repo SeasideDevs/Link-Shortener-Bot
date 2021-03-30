2531cb3784fcbf1ab3c36d382328928f9d67ffb4
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-03-30 22:05:59 +0000

    Add sharding manager

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import fs from "fs";
 import { parse } from "@iarna/toml";
-import { checks } from "./core/bootchecks";
+//import { checks } from "./core/bootchecks";
 const config = parse(
   fs.readFileSync(__dirname + "/config.toml", { encoding: "utf-8" })
 );
+import { ShardingManager } from "discord.js";
+const manager = new ShardingManager(__dirname + "/bot.js", {
+  token: process.env.TOKEN,
+});
+
+manager.spawn();
