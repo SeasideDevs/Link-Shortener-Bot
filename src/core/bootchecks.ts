9570677d0ad8e8e23b40fbd1d8a7dca5f23b9728
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-03-30 22:38:05 +0000

    Stuff

--- src/core/bootchecks.ts
+++ src/core/bootchecks.ts
@@ -1,8 +1,9 @@
-/* import { type } from "os";
+import { type } from "os";
 import chalk from "chalk";
-const { bot } = require("../bot.ts")
-export async function checks():null {
-
+const { bot } = require("../bot.js")
+const { config } = require("../index.js")
+export async function checks() {
+    config.basic.owner-ids
   return null;
 }
- */
+
