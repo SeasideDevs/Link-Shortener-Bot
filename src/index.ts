157638540bb6a835e270ead0abd23615dbba6cdd
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-03-30 00:53:54 +0000

    Create example.config.toml

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
 import { checks } from "./core/bootchecks";
-import { ShardingManager } from "discord.js";
-import { checkServerIdentity } from "tls";
-const manager = new ShardingManager("bot.ts", {
-  token: process.env.TOKEN,
-});
