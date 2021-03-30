9a1fec14827a7b84944026311933dc3cee1f49d9
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-03-30 01:19:25 +0000

    Install @iarna/toml, and do a basic read

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-import { checks } from "./core/bootchecks";
+import fs from "fs";
+// import { parse } from "@iarna/toml"
+const config = fs.readFileSync("config.toml");
+console.log(config);
+// import { checks } from "./core/bootchecks";
