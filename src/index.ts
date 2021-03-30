3a7a650302dc6f02cef49fc453f4f41f50afe83f
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-03-30 15:54:24 +0000

    Revert to parsing the the config file alone

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+import fs from "fs";
+import { parse } from "@iarna/toml";
 import { checks } from "./core/bootchecks";
-checks();
+const config = parse(
+  fs.readFileSync(__dirname + "/config.toml", { encoding: "utf-8" })
+);
