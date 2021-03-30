2e147d8c04f514d91482cc4720fe3a45318e9b07
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-03-30 22:35:02 +0000

    Finish logger

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,8 +1,62 @@
-//import chalk from "chalk";
-
-export function fatal(msg: string): null {
+import chalk from "chalk";
+type LoggerOptions = {
+  type: "fatal" | "info" | "error" | "success" | "shard" | "importantinfo";
+};
+export function log(msg: string, data: LoggerOptions): any {
   if (!msg) {
-    console.log("uwu");
+    return new TypeError("No message provided.");
   }
+  if (!data) {
+    return new TypeError("No data provided.");
+  }
+
+  const loggers = [
+    {
+      name: "fatal",
+      friendlyName: "Fatal Error",
+      log: () => {
+        console.log(chalk.bgRedBright("FATAL ERROR"), msg);
+      },
+    },
+    {
+      name: "info",
+      friendlyName: "Information",
+      log: () => {
+        console.log(chalk.whiteBright("INFO"), msg);
+      },
+    },
+    {
+      name: "error",
+      friendlyName: "Regular Error",
+      log: () => {
+        console.log(chalk.redBright("ERROR", msg));
+      },
+    },
+    {
+      name: "success",
+      friendlyName: "Success",
+      log: () => {
+        console.log(chalk.greenBright("SUCCESS"), msg);
+      },
+    },
+    {
+      name: "shard",
+      friendlyName: "Shard Info",
+      log: () => {
+        console.log(chalk.magentaBright("SHARD INFO"), msg);
+      },
+    },
+    {
+      name: "importantinfo",
+      friendlyName: "Important Info",
+      log: () => {
+        console.log(chalk.bgBlueBright("IMPORTANT INFO"), msg);
+      },
+    },
+  ];
+
+  const validTypes = loggers.map((logger) => logger.name);
+  const logData = validTypes.indexOf(data.type);
+  loggers[logData].log()
   return null;
 }
