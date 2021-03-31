3322acfde7ec649a4db4a80f622d2e47e2357a15
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-03-31 01:33:31 +0000

    Finish build script

---
+++ scripts/build.sh
@@ -0,0 +1,8 @@
+# Copy important files over
+echo "Copying important files over"
+cp src/config.toml
+# Building the actual code
+echo "Compiling files"
+npx tsc
+# Alert that the process has finished!
+echo "Done 🎉!"
