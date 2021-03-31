a7614d404bbb7fd420b37a13e8d130cebc0ce8e1
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-03-31 20:15:33 +0000

    Remove scripts cus they're broken on window

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-# Copy important files over
-echo "Copying important files over"
-cp src/config.toml build/config.toml
-# Building the actual code
-echo "Compiling files"
-npx tsc
-# Alert that the process has finished!
-echo "Done 🎉!"
--- scripts/setup.sh
+++
@@ -1,6 +0,0 @@
-# Install dependencies
-echo "Installing Dependencies"
-npm i
-# Create git hook
-echo "Creating Git Hook"
-npx husky install
