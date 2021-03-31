5362034f3d3f92830bef1a9612a64ae85a00a1c2
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-03-31 00:59:08 +0000

    Prettify file

--- src/functions/parse.ts
+++ src/functions/parse.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 import { parse } from "@iarna/toml";
 
-export function parseConfig():any {
+export function parseConfig(): any {
   return parse(
     readFileSync(__dirname + "/../config.toml", {
       encoding: "utf-8",
