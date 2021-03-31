9ed889f58be88e5e706d180eca8f5b369193162e
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-03-31 00:28:03 +0000

    Switch to using the parse function

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,6 @@
 import { Client } from "discord.js";
+import { parseConfig } from './functions/parse';
+const config = parseConfig()
 const bot = new Client({
   disableMentions: "everyone",
   ws: { intents: ["GUILDS", "GUILD_MESSAGES"] },
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import fs from "fs";
-import { parse } from "@iarna/toml";
 //import { checks } from "./core/bootchecks";
-const config = parse(
-  fs.readFileSync(__dirname + "/config.toml", { encoding: "utf-8" })
-);
+import { parseConfig } from "./functions/parse";
+const config = parseConfig();
 import { ShardingManager } from "discord.js";
 const manager = new ShardingManager(__dirname + "/bot.js", {
   token: process.env.TOKEN,
