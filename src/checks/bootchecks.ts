39426dcdab92c6a1c3f96da43a066cb1ae720135
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-04-08 00:18:10 +0000

    Add owner_ids check and some comments

--- src/checks/bootchecks.ts
+++ src/checks/bootchecks.ts
@@ -1,8 +1,17 @@
-import { type } from "os";
-import chalk from "chalk";
-const { bot } = require("../bot.js")
-const { config } = require("../index.js")
+import { log } from "../functions/logger";
+import { parseConfig } from "../functions/parse";
+const config = parseConfig();
 export async function checks() {
+  // Exit function to make it easier to exit out when something goes wrong
+  const exit = (code: number) => {
+    process.exit(code);
+  };
+  // Check if the owner_ids field has anything if not exit
+  if (!config.owner_ids.size) {
+    log("The owner field has no ID's", {
+      type: "error",
+    });
+    process.exit();
+  }
   return null;
 }
-
