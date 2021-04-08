a92d745378cc7289fe65c48da9f05d545ae3c1f7
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-04-08 00:20:56 +0000

    Add comments

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { parseConfig } from "./functions/parse";
+// Parse the config using the parseConfig() function
 const config = parseConfig();
 import { log } from "./functions/logger";
 import { ShardingManager } from "discord.js";
+// Creates a new instace of a ShardingManger
 const manager = new ShardingManager(__dirname + "/bot.js", {
   token: process.env.TOKEN,
 });
 
+// Spawn the shards
 manager.spawn();
+// Logs to console every time a shard is created
 manager.on("shardCreate", (shard) => {
   log(`Shard ${shard.id} Launched`, { type: "shard" });
 });
